cbd04935de82e4e241fad25fe1948bf44d758288
ThiagoRRamos <thiagorramos@gmail.com>
2014-11-13 12:09:43 -0200

    Changing graph maker

--- data_analysis/graph_maker.py
+++ data_analysis/graph_maker.py
@@ -4,36 +4,71 @@
 import random
 import re
 import matplotlib.pyplot as plt
+import numpy as np
 
-ree = r"(\d+) (\d+) - \((.*),(.*),(.*),(.*),(.*)\) - (\d\.\d+)"
+ree = r"(\d+) (\d+) - \((.*),(.*),(.*),(.*),(.*),(.*)\) - (\d\.\d+) (\d\.\d+) (\d\.\d+)" 
 
-f = open("../results/aa.txt", 'r')
+f = open("../results/latency_results.txt", 'r')
 
-results_l = {}
-results_p = {}
+l_results_l = {}
+l_results_p = {}
+j_results_l = {}
+j_results_p = {}
 
 for line in f.readlines():
-	d, j, mi, me, ma, dp, lo, lam = [float(x) for x in re.findall(ree, line)[0]]
-	if d not in results_l:
-		results_l[d] = 0.0
-		results_p[d] = 0.0
-	results_l[d] += lam/5
-	results_p[d] += me/5
+	d, j, mi, me, ma, dp, lo, jit, lam, jitm1, jitm2 = [float(x) for x in re.findall(ree, line)[0]]
+	if d not in l_results_l:
+		l_results_l[d] = []
+		l_results_p[d] = []
+	if j not in j_results_l:
+		j_results_l[j] = []
+		j_results_p[j] = []
+	j_results_l[j] += [jitm1 + jitm2]
+	j_results_p[j] += [jit]
+	l_results_l[d] += [lam]
+	l_results_p[d] += [me]
 
-model = [50.0*(x+1) for x in xrange(10)]
-my_r = [1000 * results_l[x/2] for x in model]
-ping_r = [results_p[x/2] for x in model]
+latency_model = [50.0*(x+1) for x in xrange(10)]
+my_latency = [1000 * sum(l_results_l[x/2]) / 25 for x in latency_model]
+my_latency_error = [1000 * np.std(l_results_l[x/2]) for x in latency_model]
+ping_latency = [sum(l_results_p[x/2]) / 25 for x in latency_model]
+ping_latency_error = [np.std(l_results_p[x/2]) for x in latency_model]
 
-def make_latency_graph(model, my_results, ping_results):
+jitter_model = [2*5.0*(x+1) for x in xrange(5)]
+my_jitter = [1000 * sum(j_results_l[x/2]) /25 for x in jitter_model]
+my_jitter_error = [1000 * np.std(j_results_l[x/2]) for x in jitter_model]
+ping_jitter = [sum(j_results_p[x/2]) /25 for x in jitter_model]
+ping_jitter_error = [np.std(j_results_p[x/2]) for x in jitter_model]
+
+def make_graph(model, my_results, ping_results, title, xlabel, ylabel):
 	plt.plot(model, my_results,'g--', label='Controlador')
 	plt.plot(model, ping_results, 'r--', label='Ping')
-	plt.title(u"Medidor de Latencia")
+	plt.title(title)
 	plt.legend()
 	plt.grid(True)
-	plt.ylabel(u'Latencia medida (ms)')
-	plt.xlabel(u'Latencia idealmente projetada (ms)')
+	plt.ylabel(ylabel)
+	plt.xlabel(xlabel)
 
+def make_graph_with_error_bars(model, my_results, ping_results, my_error, ping_error, title, xlabel, ylabel):
+	plt.errorbar(model, my_results, my_error, 0.0, 'g--', label='Controlador')
+	plt.errorbar(model, ping_results, ping_error, 0.0, 'r--', label='Ping')
+	plt.title(title)
+	plt.legend()
+	plt.grid(True)
+	plt.ylabel(ylabel)
+	plt.xlabel(xlabel)
 
-make_latency_graph(model, my_r, ping_r)
+def make_latency_graph(model, my_results, ping_results, my_error, ping_error):
+	return make_graph_with_error_bars(model, my_results, ping_results, my_error, ping_error,
+		u"Medidor de Latencia", u'Latencia idealmente projetada (ms)', u'Latencia medida (ms)')
+
+def make_jitter_graph(model, my_results, ping_results, my_error, ping_error):
+	return make_graph_with_error_bars(
+		model, my_results, ping_results, my_error, ping_error ,u"Medidor de Jitter",
+		u'Jitter idealmente projetado (ms)', u'Jitter medido (ms)')
+
+make_latency_graph(latency_model, my_latency, ping_latency, my_latency_error, ping_latency_error)
+plt.show()
+make_jitter_graph(jitter_model, my_jitter, ping_jitter, my_jitter_error, ping_jitter_error)
 plt.show()
 
